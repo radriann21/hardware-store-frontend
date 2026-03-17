6013afc6596d17e5b83a924eff193d2efff97a25
Adrian Rangel <adrianrangelargu@gmail.com>
2026-03-17 18:02:24 -0400

    refactor: improve error handler

--- src/shared/api/axiosClient.ts
+++ src/shared/api/axiosClient.ts
@@ -15,19 +15,56 @@ axiosClient.interceptors.response.use(
   (error) => {
     console.error("API Error:", error);
 
-    const isLogoutResponse = error.config.url?.includes('/auth/logout');
-
-    if (error.response?.status === 401) {
-      useAuthStore.getState().setUser(null);
-      if (!error.config.url?.includes("/auth/me") && !isLogoutResponse) {
-        toaster.create({ title: "Sesión expirada", type: "error" });
-      }
-    } else if (error.response?.status >= 500) {
+    if (!error.response) {
       toaster.create({
-        title: "Error del servidor",
-        description: "Por favor, intenta nuevamente más tarde",
+        title: "Error de conexión",
+        description: "No se pudo conectar con el servidor",
         type: "error",
       });
+      return Promise.reject(error);
+    }
+
+    const { status, data } = error.response;
+    const isLogoutResponse = error.config.url?.includes("/auth/logout");
+
+    switch (status) {
+      case 400:
+        toaster.create({
+          title: "Error de validación",
+          description: data.message || "Error de validación",
+          type: "error",
+        });
+        break;
+      case 401:
+        useAuthStore.getState().setUser(null);
+        if (!error.config.url?.includes("/auth/me") && !isLogoutResponse) {
+          toaster.create({ title: "Sesión expirada", type: "error" });
+        }
+        break;
+      case 403:
+        toaster.create({ title: "Acceso denegado", type: "error" });
+        break;
+      case 404:
+        toaster.create({ title: "Recurso no encontrado", type: "error" });
+        break;
+
+      case 500:
+      case 502:
+      case 503:
+        toaster.create({
+          title: "Error del servidor",
+          description: "Por favor, intenta nuevamente más tarde",
+          type: "error",
+        });
+        break;
+
+      default:
+        toaster.create({
+          title: "Error desconocido",
+          description: "Ocurrió un error inesperado",
+          type: "error",
+        });
+        break;
     }
 
     return Promise.reject(error);
